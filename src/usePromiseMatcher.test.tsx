ec72e26f39e59fa3499b6e34b9f6bc9e1492bbf8
Piotr Majcher <majcherpiotr.dev@gmail.com>
2020-03-29 15:14:41 +0200

    Added tests for usePromiseWithArguments

--- src/usePromiseMatcher.test.tsx
+++ src/usePromiseMatcher.test.tsx
@@ -12,7 +12,7 @@ interface TestComponent {
 }
 
 interface Params {
-    someParams: string;
+    param: string;
 }
 
 interface TestComponentWithArguments {
@@ -24,12 +24,7 @@ const ERROR_MESSAGE = "Promise was rejected";
 const SAMPLE_TEXT = "Some asynchronously loaded text";
 
 const testData: TestData = { data: SAMPLE_TEXT };
-
 const containerId = "container";
-
-const loadSomePromise = jest.fn(() => Promise.resolve(testData));
-
-const loadFailingPromise = jest.fn(() => Promise.reject(ERROR_MESSAGE));
 
 const TestComponent: React.FC<TestComponent> = ({ loader }: TestComponent) => {
     const { load, result } = usePromise<TestData>(loader);
@@ -45,10 +40,25 @@ const TestComponent: React.FC<TestComponent> = ({ loader }: TestComponent) => {
     );
 };
 
+const TestComponentWithAutoLoad: React.FC<TestComponent> = ({ loader }: TestComponent) => {
+    const { load, result } = usePromise<TestData>(loader, { autoLoad: true });
+
+    return (
+        <div data-testid={containerId}>
+            {result.match({
+                Loading: () => LOADING_MESSAGE,
+                Rejected: (err) => err,
+                Resolved: (res) => res.data,
+            })}
+            <button onClick={load}>Load</button>
+        </div>
+    );
+};
+
 const TestComponentWithArguments: React.FC<TestComponentWithArguments> = ({ loader }: TestComponentWithArguments) => {
     const { load, result } = usePromiseWithArguments<TestData, Params>(loader);
 
-    const onClick = () => load({ someParams: "someParams" });
+    const onClick = () => load({ param: SAMPLE_TEXT });
 
     return (
         <div data-testid={containerId}>
@@ -63,6 +73,14 @@ const TestComponentWithArguments: React.FC<TestComponentWithArguments> = ({ load
 };
 
 describe("usePromise", () => {
+    const loadSomePromise = jest.fn((): Promise<TestData> => Promise.resolve(testData));
+    const loadFailingPromise = jest.fn((): Promise<TestData> => Promise.reject(ERROR_MESSAGE));
+
+    afterEach(() => {
+        loadSomePromise.mockClear();
+        loadFailingPromise.mockClear();
+    });
+
     it("Loading message should be rendered by default if the promise loader function hasn't been called yet", () => {
         expect(render(<TestComponent loader={loadSomePromise} />).getByTestId(containerId)).toHaveTextContent(
             LOADING_MESSAGE,
@@ -70,17 +88,31 @@ describe("usePromise", () => {
         expect(loadSomePromise).toHaveBeenCalledTimes(0);
     });
 
-    it("Text from testData object should be rendered after the promise has been resolved", async () => {
+    it("Text from testData object should be rendered after the load button was clicked and promise has been resolved", async () => {
         const { getByTestId, getByRole } = render(<TestComponent loader={loadSomePromise} />);
 
         fireEvent.click(getByRole("button"));
+        expect(getByTestId(containerId)).toHaveTextContent(LOADING_MESSAGE);
+        expect(loadSomePromise).toHaveBeenCalledTimes(1);
         /* 
             TODO replace 'waitForElement' with 'waitFor' and update dependencies when following PRs are merged:
             - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43108
             - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43102  
         */
+        const element = await waitForElement(() => getByTestId(containerId));
+        expect(element).toHaveTextContent(SAMPLE_TEXT);
+    });
+
+    it("Text from testData object should be rendered after the promise has been resolved automatically when the 'autoLoad' flag was set to true", async () => {
+        const { getByTestId } = render(<TestComponentWithAutoLoad loader={loadSomePromise} />);
+
         expect(getByTestId(containerId)).toHaveTextContent(LOADING_MESSAGE);
         expect(loadSomePromise).toHaveBeenCalledTimes(1);
+        /* 
+            TODO replace 'waitForElement' with 'waitFor' and update dependencies when following PRs are merged:
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43108
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43102  
+        */
         const element = await waitForElement(() => getByTestId(containerId));
         expect(element).toHaveTextContent(SAMPLE_TEXT);
     });
@@ -90,7 +122,56 @@ describe("usePromise", () => {
 
         fireEvent.click(getByRole("button"));
         expect(getByTestId(containerId)).toHaveTextContent(LOADING_MESSAGE);
+        expect(loadFailingPromise).toHaveBeenCalledTimes(1);
+        /* 
+            TODO replace 'waitForElement' with 'waitFor' and update dependencies when following PRs are merged:
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43108
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43102  
+        */
+        const element = await waitForElement(() => getByTestId(containerId));
+        expect(element).toHaveTextContent(ERROR_MESSAGE);
+    });
+});
+
+describe("usePromiseWithArguments", () => {
+    const loadSomePromise = jest.fn(
+        (params: Params): Promise<TestData> => Promise.resolve<TestData>({ data: params.param }),
+    );
+    const loadFailingPromise = jest.fn((): Promise<TestData> => Promise.reject(ERROR_MESSAGE));
+
+    afterEach(() => {
+        loadSomePromise.mockClear();
+        loadFailingPromise.mockClear();
+    });
+
+    it("Loading message should be rendered by default if the promise loader function hasn't been called yet", () => {
+        expect(
+            render(<TestComponentWithArguments loader={loadSomePromise} />).getByTestId(containerId),
+        ).toHaveTextContent(LOADING_MESSAGE);
+        expect(loadSomePromise).toHaveBeenCalledTimes(0);
+    });
+
+    it("Text from testData object should be rendered after the load button was clicked and promise has been resolved", async () => {
+        const { getByTestId, getByRole } = render(<TestComponentWithArguments loader={loadSomePromise} />);
+
+        fireEvent.click(getByRole("button"));
+        expect(getByTestId(containerId)).toHaveTextContent(LOADING_MESSAGE);
         expect(loadSomePromise).toHaveBeenCalledTimes(1);
+        /* 
+            TODO replace 'waitForElement' with 'waitFor' and update dependencies when following PRs are merged:
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43108
+            - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43102  
+        */
+        const element = await waitForElement(() => getByTestId(containerId));
+        expect(element).toHaveTextContent(SAMPLE_TEXT);
+    });
+
+    it("Error message should be rendered after the promise has been rejected", async () => {
+        const { getByTestId, getByRole } = render(<TestComponentWithArguments loader={loadFailingPromise} />);
+
+        fireEvent.click(getByRole("button"));
+        expect(getByTestId(containerId)).toHaveTextContent(LOADING_MESSAGE);
+        expect(loadFailingPromise).toHaveBeenCalledTimes(1);
         /* 
             TODO replace 'waitForElement' with 'waitFor' and update dependencies when following PRs are merged:
             - https://github.com/DefinitelyTyped/DefinitelyTyped/pull/43108
