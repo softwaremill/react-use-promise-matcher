222b1f47d2dd292e46f2790adbb238b5dbac7e51
Piotr Majcher <majcherpiotr.dev@gmail.com>
2020-04-02 02:59:28 +0200

    Updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
-import resolve from "rollup-plugin-node-resolve";
+// import resolve from "rollup-plugin-node-resolve";
 import sourceMaps from "rollup-plugin-sourcemaps";
-import camelCase from "lodash.camelcase";
 import typescript from "rollup-plugin-typescript2";
 import json from "rollup-plugin-json";
 // import react from "react";
@@ -16,15 +15,6 @@ export default {
     output: [
         {
             file: pkg.main,
-            name: camelCase(libraryName),
-            format: "umd",
-            sourcemap: true,
-            globals: {
-                react: "React",
-            },
-        },
-        {
-            file: pkg.module,
             format: "es",
             sourcemap: true,
             globals: {
@@ -36,39 +26,39 @@ export default {
     external: ["react"],
 
     plugins: [
-        resolve({
-            // the fields to scan in a package.json to determine the entry point
-            // if this list contains "browser", overrides specified in "pkg.browser"
-            // will be used
-            mainFields: ["module", "main"], // Default: ['module', 'main']
+        // resolve({
+        //     // the fields to scan in a package.json to determine the entry point
+        //     // if this list contains "browser", overrides specified in "pkg.browser"
+        //     // will be used
+        //     mainFields: ["module", "main"], // Default: ['module', 'main']
 
-            // not all files you want to resolve are .js files
-            extensions: [".ts", ".tsx"], // Default: [ '.mjs', '.js', '.json', '.node' ]
+        //     // not all files you want to resolve are .js files
+        //     extensions: [".ts", ".tsx"], // Default: [ '.mjs', '.js', '.json', '.node' ]
 
-            // Lock the module search in this path (like a chroot). Module defined
-            // outside this path will be marked as external
-            jail: "./src/", // Default: '/'
+        //     // Lock the module search in this path (like a chroot). Module defined
+        //     // outside this path will be marked as external
+        //     jail: "./src/", // Default: '/'
 
-            // Set to an array of strings and/or regexps to lock the module search
-            // to modules that match at least one entry. Modules not matching any
-            // entry will be marked as external
-            // only: ["some_module", /^@some_scope\/.*$/], // Default: null
+        //     // Set to an array of strings and/or regexps to lock the module search
+        //     // to modules that match at least one entry. Modules not matching any
+        //     // entry will be marked as external
+        //     // only: ["some_module", /^@some_scope\/.*$/], // Default: null
 
-            // If true, inspect resolved files to check that they are
-            // ES2015 modules
-            modulesOnly: true, // Default: false
+        //     // If true, inspect resolved files to check that they are
+        //     // ES2015 modules
+        //     modulesOnly: true, // Default: false
 
-            // Force resolving for these modules to root's node_modules that helps
-            // to prevent bundling the same package multiple times if package is
-            // imported from dependencies.
-            dedupe: ["react", "react-dom"], // Default: []
+        //     // Force resolving for these modules to root's node_modules that helps
+        //     // to prevent bundling the same package multiple times if package is
+        //     // imported from dependencies.
+        //     dedupe: ["react", "react-dom"], // Default: []
 
-            // Any additional options that should be passed through
-            // to node-resolve
-            customResolveOptions: {
-                moduleDirectory: "js_modules",
-            },
-        }),
+        //     // Any additional options that should be passed through
+        //     // to node-resolve
+        //     customResolveOptions: {
+        //         moduleDirectory: "js_modules",
+        //     },
+        // }),
         // Allow json resolution
         json(),
         // Compile TypeScript files
