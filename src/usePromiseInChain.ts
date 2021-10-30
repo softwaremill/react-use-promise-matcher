f5e5e0237e6ab9f90ef32a0a3b8b2c80298b2cfa
afternoon2 <poprostuantolak@gmail.com>
2021-10-30 17:53:24 +0200

    refactor(*): remove unnecessary file

--- src/usePromiseInChain.ts
+++
@@ -1,6 +0,0 @@
-import { PromiseLoader } from "./types";
-import { usePromise } from "./usePromiseMatcher";
-
-export const usePromiseInChain = <T, Args extends any[], E = string>(loaderFn: PromiseLoader<T, Args>) => {
-    const [result, load, reset] = usePromise<T, Args, E>(loaderFn);
-};
